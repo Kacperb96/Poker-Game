8cfa627af901b572e08465f014b40f410b28e746
Kacperb96 <kacperb1996@icloud.com>
2022-04-27 19:13:27 +0200

    Refactoring

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@
 class DeckOfCardsFixture : public testing::Test{
     public:
         void SetUp();
+        void changeCardFromSet(CardSet&, Card&);
+
     protected:
         Card c1, c2, c3, c4, c5;
         CardSet straightSet;
@@ -15,6 +17,11 @@ class DeckOfCardsFixture : public testing::Test{
         CardSet quadsSet;
         CardSet straightFlushSet;
 };
+
+void DeckOfCardsFixture::changeCardFromSet(CardSet& cardSet, Card& c){
+    cardSet.pop_back();
+    cardSet.push_back(c);
+}
 
 void DeckOfCardsFixture::SetUp(){
     straightFlushSet = {std::make_pair(Figures::Nine, Colour::Spades), std::make_pair(Figures::Six, Colour::Spades),
@@ -45,78 +52,56 @@ void DeckOfCardsFixture::SetUp(){
 TEST_F(DeckOfCardsFixture, IsTwoPair){
     EXPECT_EQ(twoPair(twoPairSet), true);
     Card c = std::make_pair(Figures::King, Colour::Hearts);
-
-    twoPairSet.pop_back();
-    twoPairSet.push_back(c);
-
+    changeCardFromSet(twoPairSet, c);
     EXPECT_EQ(twoPair(twoPairSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsOnePair){
     EXPECT_EQ(onePair(onePairSet), true);
     Card c = std::make_pair(Figures::Jack, Colour::Diamonds);
-    
-    onePairSet.pop_back();
-    onePairSet.push_back(c);
-
+    changeCardFromSet(onePairSet, c);
     EXPECT_EQ(onePair(onePairSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsThreeOfAKind){
     EXPECT_EQ(threeOfAKind(threeOfaKindSet), true);
     Card c = std::make_pair(Figures::Three, Colour::Diamonds);
-
-    threeOfaKindSet.pop_back();
-    threeOfaKindSet.push_back(c);
-
+    changeCardFromSet(threeOfaKindSet, c);
     EXPECT_EQ(threeOfAKind(threeOfaKindSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsFlush){
     EXPECT_EQ(flush(flushSet), true);
     Card c = std::make_pair(Figures::Six, Colour::Diamonds);
-
-    flushSet.pop_back();
-    flushSet.push_back(c);
-    
+    changeCardFromSet(flushSet, c);
     EXPECT_EQ(flush(flushSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsStraight){
     EXPECT_EQ(straight(straightSet), true);
     Card c = std::make_pair(Figures::King, Colour::Diamonds);
-
-    straightSet.pop_back();
-    straightSet.push_back(c);
-    
+    changeCardFromSet(straightSet, c); 
     EXPECT_EQ(straight(straightSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsFullHouse){
     EXPECT_EQ(fullHouse(fullHouseSet), true);
     Card c = std::make_pair(Figures::Ace, Colour::Spades);
-
-    fullHouseSet.pop_back();
-    fullHouseSet.push_back(c);
-
+    changeCardFromSet(fullHouseSet, c);
     EXPECT_EQ(fullHouse(fullHouseSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsQuads){
     EXPECT_EQ(quads(quadsSet), true);
     Card c = std::make_pair(Figures::Queen, Colour::Spades);
-
-    quadsSet.pop_back();
-    quadsSet.push_back(c);
+    changeCardFromSet(quadsSet, c);
     EXPECT_EQ(quads(quadsSet), false);
 }
 
 TEST_F(DeckOfCardsFixture, IsStraightFlush){
     EXPECT_EQ(straightFlush(straightFlushSet), true);
     Card c = std::make_pair(Figures::Queen, Colour::Spades);
-
-    straightFlushSet.pop_back();
-    straightFlushSet.push_back(c);
+    changeCardFromSet(straightFlushSet, c);
     EXPECT_EQ(straightFlush(straightFlushSet), false);
 }
 
