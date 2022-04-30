323c4fd4370b8aaf55fe9a8ab79b9fc551032340
Kacperb96 <kacperb1996@icloud.com>
2022-04-30 22:01:49 +0200

    Refactoring

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@ int main(){
     CardSet cardDeck = creatingCardDeck();
     
     drawCards(cardSet1, cardDeck);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     drawCards(cardSet2, cardDeck);
     std::cout << "First set" << std::endl;
     printCardSet(cardSet1);
     std::cout << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "Second set" << std::endl;
     printCardSet(cardSet2);
+    std::cout << "\n";
+    
+    for(int i = 0; i < cardDeck.size(); i++){
+        std::cout << cardDeck[i].first << cardDeck[i].second << " ";
+    }
     std::cout << "\n";
 
     return 0;
